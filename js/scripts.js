b37e048c5c66642dc10a4e0576267f05bbd31b19
Jeremiah Freeman and Zach Swanson <jeremiah@>
2017-01-19 14:32:30 -0800

    add user interface, form and event handler

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // back end logic
 
-var test = "1978";
+var test = "2963";
 var romanNumerals = ["I","V","X","L","C","D","M"];
 
 var buildTestArray = function(string){
@@ -22,37 +22,42 @@ var writeNumeral = function(number,place){
     }
   } else if (number === 4){
     numeral = romanNumerals[place]+romanNumerals[place+1]
-  } else if (number>4 && number<9) {
+  } else if ((number>4) && (number<9)) {
     numeral = romanNumerals[place+1];
     for (var i=0;i<(number-5);i+=1) {
       numeral = numeral + romanNumerals[place]
     }
-  } else if (number > 9) {
+  } else if (number > 8) {
     numeral = romanNumerals[place]+romanNumerals[place+2];
   }
   return numeral;
 };
-// alert(writeNumeral(5,0));
-// alert(writeNumeral(5,2));
-// alert(writeNumeral(9,0));
-// alert(writeNumeral(9,2));
 
 var romanNum = function (string) {
   var numArray = buildTestArray(string);
   var numArray = numArray.reverse();
-  console.log(numArray.toString());
   var place = 0;
   var output="";
   for (var r=0;r<4;r+=1) {
     output = writeNumeral(numArray[r], place) + output;
-    console.log(place);
     place += 2;
-    console.log(r);
-    console.log(numArray[r])
-    console.log(output);
   }
   return output;
 };
 
+$(document).ready(function() {
+  $("#input").submit(function(event) {
+    event.preventDefault();
+    alert("what!");
 
-alert(romanNum(test));
+    var input = $(".number-input").val();
+    console.log(output);
+
+    var output = romanNum(input);
+
+    $("#output").text(output);
+  });
+
+
+
+});
